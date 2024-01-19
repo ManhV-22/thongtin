d760a76c577f6a086423715ada7acd2ea877a2c0
ManhV-22 <20232652@eaut.edu.vn>
2024-01-19 15:18:49 +0700

    upload code

---
+++ infoTeam/main.js
@@ -0,0 +1,12 @@
+function fbDangXuanManh(){
+    window.open('https://www.facebook.com/DangXuanManh.2005');
+}
+
+function fbHoangVanThang(){
+    window.open('https://www.facebook.com/thang.hoang.van.1509');
+}
+
+function fbTranNgocSon(){
+    window.open('https://www.facebook.com/sonngoctran3110');
+}
+
